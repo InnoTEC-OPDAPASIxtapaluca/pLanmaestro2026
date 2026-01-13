a57239c1691324cfb6f31cbdea6016648b391c77
InnoTEC-OPDAPASIxtapaluca <innovacion.tecnologica.2024@gmail.com>
2026-01-13 15:14:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ Papa.parse("datos.csv", {
 
       if (geom.type === "POLYGON") {
         layer = L.polygon(geom.coords, {
-          color: "#ff9900",      // borde
+          color: "#ffFFFF",      // borde
           fillColor: "#ffcc66",  // relleno
           fillOpacity: 0.5,
           weight: 2
